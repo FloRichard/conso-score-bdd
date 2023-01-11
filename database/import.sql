2308d6b946e83df4606b84ecd35a215b09827f64
frichard <florian.richard@ecole.ensicaen.fr>
2023-01-11 09:32:18 +0100

    fix script

---
+++ database/import.sql
@@ -0,0 +1,4 @@
+insert into public.user (user_id, status_id, name, login, password) values ("b550312d-b519-4083-8440-34af067d7949", "7ec56444-6df0-4f8a-a154-822858d990af", "Jean Dupont", "jeanDupont", "mypassword");
+insert into public.seller(seller_id, name, location, seller_stock_id) values ("");
+insert into public.status (status_id, maker_id, seller_id, label) values ("7ec56444-6df0-4f8a-a154-822858d990af", null, "5a39c805-4e15-4944-80af-333d2d11bced", "MAKER");
+
