c732147e5748e4051979e48bfd09564020fa19d6
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2023-01-12 16:14:13 +0100

    update db scripts

--- database/set_up_db_2.sh
+++ database/set_up_db_2.sh
@@ -2,13 +2,14 @@
 
 cd $HOME/dev/database/conso-score || exit
 
-default_db = "postgres"
-db = "consoscore"
+default_db="postgres"
+db="consoscore"
 
 psql -d "${default_db}" -c "create database ${db};"
+
 psql -d "${default_db}" -c "create user conso_score_user with encrypted password 'conso_score_user_pwd';"
 
-files=("scheme.sql" "import.sql" "user.sql")
+files=( "scheme.sql" "import.sql" "user.sql" )
 
 for file in "${files[@]}"
 do
